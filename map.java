a833123cb922a8b013b22c7feab0635121445332
Ilhom <34709366+ilhomelv@users.noreply.github.com>
2018-01-07 13:16:36 +0500

    Update map.java

--- map.java
+++ map.java
@@ -16,6 +16,32 @@ int size()
 void clear() 
 
 
+Collection Views of Map objects 
+Set KeySet()			views only keys, return type is Set
+Collection values()		            views only values, return type is Collection
+Set entrySet()			views entry objects, return type is Set
+***output comes in { } curly braces
+
+Entry Interface Methods
+Object getKey()
+Object getValue()
+Object setValue(Object o)
+***output comes in square brackets [ ]
+	
+            
+HashMap is a child of Map, underlying data structure is HashTable
+	insertion order Not preserved and its based on Hash codes of keys, 
+	Duplicate Keys are NOT allowed, Duplicate values OK, Heterogeneous OK,
+	Null is OK for keys once, for values infinite, implements Serializable and Cloneable.
+	Great for Searching operations
+
+HashMap Constructors
+HashMap m =new HashMap(); 	//default init capacity 16, fill ratio 0.75
+HashMap m =new HashMap(int initCapacity);
+HashMap m =new HashMap(int initCap, float fillRatio);
+HashMap m =new HashMap(Map m);
+
+
 
 
 */
