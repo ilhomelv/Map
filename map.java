62e38ae6d0ef66d303259ea1727be324c533f029
Ilhom <34709366+ilhomelv@users.noreply.github.com>
2018-01-07 12:50:54 +0500

    Create map.java

---
+++ map.java
@@ -0,0 +1,21 @@
+/*
+Map Interface 	
+            is NOT a child of Collection,
+            Each key-value pair objects known as Entry,NO key duplicates allowed for insertion,
+            value duplicates are OK.
+
+MAP methods
+Object put(Object key, Object value)    put key-value pair into Map
+void putAll(Map m)				
+Object get(Object key)                  returns the value associated with the key
+Object remove(Object key)               removes the entry pair assoc. with the key
+boolean containsKey(Object key)
+boolean containsValue(Object value)
+boolean isEmpty()
+int size()
+void clear() 
+
+
+
+
+*/
